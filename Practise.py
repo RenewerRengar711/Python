03868ad3a08a2316a830d0f73c900dec83907ce7
RenewerRengar711 <39066153+RenewerRengar711@users.noreply.github.com>
2019-11-27 15:33:47 +0200

    Add files via upload

--- Practise.py
+++ Practise.py
@@ -1,33 +1,38 @@
 import tkinter as tk
 from tkinter import font as tkfont
+# Basic imports of files need for tkinter
 
 
+# This class is the complete app and their windows
 class FirstOne(tk.Tk):
 
     def __init__(self, *args, **kwargs):
 
         tk.Tk.__init__(self, *args, **kwargs)
         self.title_font = tkfont.Font(family="helevetica", size=18, weight="bold", slant="italic")
-
+# A container for the frame that is being used
         container = tk.Frame(self)
         container.pack(side='top', fill='both', expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
+# A list of all the frames that there is to choose from
         self.frames = {}
         for F in (StartPage, ):
+            # page_name is just the frame that is chosen to be used
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
             frame.grid(row=0, column=0, sticky="nsew")
-
+# Runs the function made to show frame
         self.show_frame("StartPage")
+# Function to show frame
 
     def show_frame(self, page_name):
         frame = self.frames[page_name]
         frame.tkraise()
 
 
+# the start page3
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -37,6 +42,7 @@ class StartPage(tk.Frame):
         l1.pack(side='top')
 
 
+# Just to run the entire app
 if __name__ == "__main__":
     App = FirstOne()
     App.geometry("500x500")
